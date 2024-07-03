f20b438f41fb0ea85b1fe7dc57fdeae78e4b9ba9
Sunicg <Sunilcg2506@gmail.com>
2024-07-03 13:38:22 +0530

    updated new bucket name for storing the eks statefile

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-sunil" # Replace with your actual S3 bucket name
+    bucket = "eks-terraform-bucket-suni" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
