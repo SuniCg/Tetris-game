2cc7e67e872032fad17e737307394bfddd337e00
Sunicg <Sunilcg2506@gmail.com>
2024-07-04 10:12:30 +0530

    updated eks backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "eks-terraform-bucket-suni" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "Eks/terraform.tfstate"
     region = "ap-south-1"
   }
 }
